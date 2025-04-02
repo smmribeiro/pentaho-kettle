0696e60773071f675689d9100d4d4c265c9c97a0
Sergio Ribeiro <sergio.ribeiro@hitachivantara.com>
2025-04-02 18:38:10 +0100

    fix: DefaultFileSystemConfigBuilder.setUserAuthenticator does not throw Exceptions with 2.10.0 vfs2 [PPP-5649]

--- core/src/main/java/org/pentaho/di/connections/vfs/providers/other/OtherConnectionDetailsProvider.java
+++ core/src/main/java/org/pentaho/di/connections/vfs/providers/other/OtherConnectionDetailsProvider.java
@@ -49,11 +49,9 @@ public class OtherConnectionDetailsProvider extends BaseVFSConnectionProvider<Ot
         getVar( otherConnectionDetails.getUsername(), space ),
         getVar( otherConnectionDetails.getPassword(), space ) );
     FileSystemOptions opts = new FileSystemOptions();
-    try {
-      DefaultFileSystemConfigBuilder.getInstance().setUserAuthenticator( opts, auth );
-    } catch ( FileSystemException fse ) {
-      // Ignore and return default options
-    }
+
+    DefaultFileSystemConfigBuilder.getInstance().setUserAuthenticator( opts, auth );
+
     return opts;
   }
 
